3553471da73549c838636c1c5fd73e395250b51c
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-07-31 14:48:28 +0530

    Update gforce.py

--- gforce.py
+++ gforce.py
@@ -10,19 +10,13 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from PyPDF2 import PdfReader
+import openai
 
-import streamlit as st
-import os
-import PyPDF2
-from langchain.llms import OpenAI
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
-from PyPDF2 import PdfReader
+# Set up your OpenAI API key
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 def read_pdf_text(uploaded_file):
-    pdf_reader = PdfReader(uploaded_file)
+    pdf_reader = PyPDF2.PdfReader(uploaded_file)
     text = ""
 
     for page in pdf_reader.pages:
@@ -30,63 +24,49 @@ def read_pdf_text(uploaded_file):
 
     return text
 
-def generate_response(uploaded_file, openai_api_key, conversation_history):
-    # Load document if file is uploaded
-    if uploaded_file is not None:
-        documents = [read_pdf_text(uploaded_file)]
-        # Split documents into chunks
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        texts = text_splitter.create_documents(documents)
-        # Select embeddings
-        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-        # Create a vectorstore from documents
-        db = Chroma.from_documents(texts, embeddings)
-        # Create retriever interface
-        retriever = db.as_retriever()
-        # Create QA chain
-        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
-
-        # Handle multiple queries and store conversation history
-        for user_query in conversation_history:
-            response = qa.run(user_query)
-            conversation_history.append((user_query, response))
-
-        return conversation_history
-
 # Page title
 st.set_page_config(page_title='GForce Resume Reader')
 st.title('GForce Resume Reader')
 
 # File upload
-uploaded_file = st.file_uploader('Please upload you resume', type='pdf')
+uploaded_file = st.file_uploader('Please upload your resume', type='pdf')
 
 # Retrieve or initialize conversation history
 if 'conversation_history' not in st.session_state:
     st.session_state.conversation_history = []
+
+# Read the PDF content and set it as the initial context for the chatbot
+if uploaded_file is not None:
+    initial_context = read_pdf_text(uploaded_file)
+    st.session_state.conversation_history = [{'role': 'system', 'content': initial_context}]
 
 # User query
 query_text = st.text_input('How can I help?:', value='', key='user_input', help='Ask away!', type='default')
 
 # Form input and query
 with st.form('myform', clear_on_submit=True):
-    openai_api_key = st.secrets["OPENAI_API_KEY"]
     submitted = st.form_submit_button('Submit', key='submit_button', help='Click to submit the query')
-    if submitted and openai_api_key.startswith('sk-'):
+    if submitted and query_text.strip() != '':
         with st.spinner('Loading response...'):
             # Add the user query to the conversation history
-            st.session_state.conversation_history.append(query_text)
+            st.session_state.conversation_history.append({'role': 'user', 'content': query_text})
             # Get the updated conversation history
-            conversation_history = st.session_state.conversation_history.copy()
-            # Generate the response using the updated conversation history
-            result = generate_response(uploaded_file, openai_api_key, conversation_history)
-    else:
-        result = []
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=st.session_state.conversation_history,
+                api_key=openai_api_key
+            )
+            # Get the assistant's response
+            assistant_response = response['choices'][0]['message']['content']
+            # Append the assistant's response to the conversation history
+            st.session_state.conversation_history.append({'role': 'assistant', 'content': assistant_response})
 
 # Display the conversation history
-if result:
+if len(st.session_state.conversation_history) > 1:
     st.header('Conversation History:')
-    for user_query, response in result:
-        st.subheader('User:')
-        st.write(user_query)
-        st.subheader('Chatbot:')
-        st.write(response)
+    for message in st.session_state.conversation_history[1:]:  # Skip the initial context message
+        if message['role'] == 'user':
+            st.subheader('User:')
+        elif message['role'] == 'assistant':
+            st.subheader('Chatbot:')
+        st.write(message['content'])
