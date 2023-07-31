ff2401f0dd5a9a5d65b742f2910278df7563a3c5
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-07-31 16:25:55 +0530

    Update gforce.py

--- gforce.py
+++ gforce.py
@@ -40,23 +40,13 @@ if uploaded_file is not None:
     initial_context = read_pdf_text(uploaded_file)
     st.session_state.conversation_history = [{'role': 'system', 'content': initial_context}]
 
-# Display the entire conversation history with chat bubbles
-st.header('Conversation History:')
-if st.session_state.conversation_history:
-    for message in st.session_state.conversation_history:
-        if message['role'] == 'user':
-            st.markdown(f'<div style="display: block; text-align: left; padding: 5px; background-color: #e0e0e0; border-radius: 10px; margin-bottom: 5px; width: 50%;">{message["content"]}</div>', unsafe_allow_html=True)
-        elif message['role'] == 'assistant':
-            st.markdown(f'<div style="display: block; text-align: right; padding: 5px; background-color: #0078d4; color: white; border-radius: 10px; margin-bottom: 5px; width: 50%;">{message["content"]}</div>', unsafe_allow_html=True)
-
 # User query
 query_text = st.text_input('You (Type your message here):', value='', help='Ask away!', type='default')
 
 # Form input and query
-form_submitted = False
 with st.form('myform', clear_on_submit=True):
-    form_submitted = st.form_submit_button('Send', help='Click to submit the query')
-    if query_text.strip() != '' and form_submitted:
+    st.form_submit_button('Send', help='Click to submit the query')
+    if query_text.strip() != '':
         with st.spinner('Chatbot is typing...'):
             # Add the user query to the conversation history
             st.session_state.conversation_history.append({'role': 'user', 'content': query_text})
@@ -73,6 +63,18 @@ with st.form('myform', clear_on_submit=True):
             # Append the assistant's response to the conversation history
             st.session_state.conversation_history.append({'role': 'assistant', 'content': assistant_response})
 
+# Display the entire conversation history with chat bubbles
+if st.session_state.conversation_history:
+    st.header('Conversation History:')
+    chat_history_placeholder = st.empty()  # Placeholder to store chat history
+
+    for message in st.session_state.conversation_history:
+        if message['role'] == 'user':
+            st.markdown(f'<div style="display: block; text-align: left; padding: 5px; background-color: #e0e0e0; border-radius: 10px; margin-bottom: 5px; width: 50%;">{message["content"]}</div>', unsafe_allow_html=True)
+        elif message['role'] == 'assistant':
+            st.markdown(f'<div style="display: block; text-align: right; padding: 5px; background-color: #0078d4; color: white; border-radius: 10px; margin-bottom: 5px; width: 50%;">{message["content"]}</div>', unsafe_allow_html=True)
+
 # Add a clear conversation button
 if st.button('Clear Conversation'):
     st.session_state.conversation_history.clear()
+    
