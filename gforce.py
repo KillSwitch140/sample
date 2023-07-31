1dea4d23ac6bb6e71e0fcf71ee40df38cb8026c0
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-07-31 13:27:40 +0530

    Update gforce.py

--- gforce.py
+++ gforce.py
@@ -60,7 +60,21 @@ with st.form('myform', clear_on_submit=True):
 
 if len(result):
     st.info(response)
-
+with st.sidebar:
+        st.markdown(
+            "## How to use\n"
+            "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"  # noqa: E501
+            "2. Upload a pdf, docx, or txt file📄\n"
+            "3. Ask a question about the document💬\n"
+        )
+        api_key_input = st.text_input(
+            "OpenAI API Key",
+            type="password",
+            placeholder="Paste your OpenAI API key here (sk-...)",
+            help="You can get your API key from https://platform.openai.com/account/api-keys.",  # noqa: E501
+            value=os.environ.get("OPENAI_API_KEY", None)
+            or st.session_state.get("OPENAI_API_KEY", ""),
+        )
 
         
 
