64b430feba18b2b125000ae8d0b245d0443f87ae
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-07-31 16:01:26 +0530

    Update gforce.py

--- gforce.py
+++ gforce.py
@@ -39,17 +39,6 @@ if uploaded_file is not None:
     initial_context = read_pdf_text(uploaded_file)
     st.session_state.conversation_history = [{'role': 'system', 'content': initial_context}]
 
-# Display the entire conversation history with chat bubbles
-if st.session_state.conversation_history:
-    st.header('Conversation History:')
-    chat_history_placeholder = st.empty()  # Placeholder to store chat history
-
-    for message in st.session_state.conversation_history:
-        if message['role'] == 'user':
-            st.markdown(f'<div style="display: block; text-align: left; padding: 5px; background-color: #e0e0e0; border-radius: 10px; margin-bottom: 5px; width: 50%;">{message["content"]}</div>', unsafe_allow_html=True)
-        elif message['role'] == 'assistant':
-            st.markdown(f'<div style="display: block; text-align: right; padding: 5px; background-color: #0078d4; color: white; border-radius: 10px; margin-bottom: 5px; width: 50%;">{message["content"]}</div>', unsafe_allow_html=True)
-
 # User query
 query_text = st.text_input('You (Type your message here):', value='', help='Ask away!', type='default')
 
@@ -72,6 +61,17 @@ with st.form('myform', clear_on_submit=True):
             assistant_response = response['choices'][0]['message']['content']
             # Append the assistant's response to the conversation history
             st.session_state.conversation_history.append({'role': 'assistant', 'content': assistant_response})
+
+    # Display the entire conversation history with chat bubbles
+    if st.session_state.conversation_history:
+        st.header('Conversation History:')
+        chat_history_placeholder = st.empty()  # Placeholder to store chat history
+
+        for message in st.session_state.conversation_history:
+            if message['role'] == 'user':
+                chat_history_placeholder.markdown(f'<div style="display: block; text-align: left; padding: 5px; background-color: #e0e0e0; border-radius: 10px; margin-bottom: 5px; width: 50%;">{message["content"]}</div>', unsafe_allow_html=True)
+            elif message['role'] == 'assistant':
+                chat_history_placeholder.markdown(f'<div style="display: block; text-align: right; padding: 5px; background-color: #0078d4; color: white; border-radius: 10px; margin-bottom: 5px; width: 50%;">{message["content"]}</div>', unsafe_allow_html=True)
 
 # Add a clear conversation button
 if st.button('Clear Conversation'):
