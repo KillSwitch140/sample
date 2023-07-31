0f01c31743f27291f24fb4730c50d3b312bff94e
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-07-31 16:29:39 +0530

    Update gforce.py

--- gforce.py
+++ gforce.py
@@ -41,40 +41,40 @@ if uploaded_file is not None:
     st.session_state.conversation_history = [{'role': 'system', 'content': initial_context}]
 
 # User query
-query_text = st.text_input('You (Type your message here):', value='', help='Ask away!', type='default')
+query_text = st.text_input('You:', value='', help='Ask away!', type='default')
 
 # Form input and query
 with st.form('myform', clear_on_submit=True):
-    st.form_submit_button('Send', help='Click to submit the query')
+    st.form_submit_button('Send', help='Click to send the message')
     if query_text.strip() != '':
-        with st.spinner('Chatbot is typing...'):
-            # Add the user query to the conversation history
-            st.session_state.conversation_history.append({'role': 'user', 'content': query_text})
-            # Get the updated conversation history
-            conversation_history = st.session_state.conversation_history.copy()
-            # Generate the response using the updated conversation history
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=conversation_history,
-                api_key=openai_api_key
-            )
-            # Get the assistant's response
-            assistant_response = response['choices'][0]['message']['content']
-            # Append the assistant's response to the conversation history
-            st.session_state.conversation_history.append({'role': 'assistant', 'content': assistant_response})
+        # Add the user query to the conversation history
+        st.session_state.conversation_history.append({'role': 'user', 'content': query_text})
+        # Get the updated conversation history
+        conversation_history = st.session_state.conversation_history.copy()
+        # Generate the response using the updated conversation history
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=conversation_history,
+            api_key=openai_api_key
+        )
+        # Get the assistant's response
+        assistant_response = response['choices'][0]['message']['content']
+        # Append the assistant's response to the conversation history
+        st.session_state.conversation_history.append({'role': 'assistant', 'content': assistant_response})
 
 # Display the entire conversation history with chat bubbles
 if st.session_state.conversation_history:
-    st.header('Conversation History:')
+    st.header('Chat History:')
     chat_history_placeholder = st.empty()  # Placeholder to store chat history
 
     for message in st.session_state.conversation_history:
         if message['role'] == 'user':
-            st.markdown(f'<div style="display: block; text-align: left; padding: 5px; background-color: #e0e0e0; border-radius: 10px; margin-bottom: 5px; width: 50%;">{message["content"]}</div>', unsafe_allow_html=True)
+            # User message on the left side
+            chat_history_placeholder.markdown(f'<div style="display: flex; flex-direction: row; justify-content: flex-start;"><div style="background-color: #f2f2f2; border-radius: 10px; padding: 10px; margin-bottom: 10px; max-width: 70%; word-wrap: break-word;">{message["content"]}</div></div>', unsafe_allow_html=True)
         elif message['role'] == 'assistant':
-            st.markdown(f'<div style="display: block; text-align: right; padding: 5px; background-color: #0078d4; color: white; border-radius: 10px; margin-bottom: 5px; width: 50%;">{message["content"]}</div>', unsafe_allow_html=True)
+            # Chatbot message on the right side
+            chat_history_placeholder.markdown(f'<div style="display: flex; flex-direction: row; justify-content: flex-end;"><div style="background-color: #0078d4; color: white; border-radius: 10px; padding: 10px; margin-bottom: 10px; max-width: 70%; word-wrap: break-word;">{message["content"]}</div></div>', unsafe_allow_html=True)
 
 # Add a clear conversation button
 if st.button('Clear Conversation'):
     st.session_state.conversation_history.clear()
-    
