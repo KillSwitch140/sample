498a075706adfaa45811833f92a52a45fe221c5d
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-07-31 14:42:52 +0530

    Update gforce.py

--- gforce.py
+++ gforce.py
@@ -11,6 +11,15 @@ from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from PyPDF2 import PdfReader
 
+import streamlit as st
+import os
+import PyPDF2
+from langchain.llms import OpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from PyPDF2 import PdfReader
 
 def read_pdf_text(uploaded_file):
     pdf_reader = PdfReader(uploaded_file)
@@ -18,8 +27,9 @@ def read_pdf_text(uploaded_file):
 
     for page in pdf_reader.pages:
         text += page.extract_text()
+
     return text
-    
+
 def generate_response(uploaded_file, openai_api_key, conversation_history):
     # Load document if file is uploaded
     if uploaded_file is not None:
@@ -35,12 +45,12 @@ def generate_response(uploaded_file, openai_api_key, conversation_history):
         retriever = db.as_retriever()
         # Create QA chain
         qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
-        
+
         # Handle multiple queries and store conversation history
         for user_query in conversation_history:
             response = qa.run(user_query)
             conversation_history.append((user_query, response))
-        
+
         return conversation_history
 
 # Page title
@@ -55,12 +65,12 @@ if 'conversation_history' not in st.session_state:
     st.session_state.conversation_history = []
 
 # User query
-query_text = st.text_input('How can I help?:', placeholder='Ask away!', disabled=not uploaded_file)
+query_text = st.text_input('How can I help?:', value='', key='user_input', help='Ask away!', type='default')
 
 # Form input and query
 with st.form('myform', clear_on_submit=True):
     openai_api_key = st.secrets["OPENAI_API_KEY"]
-    submitted = st.form_submit_button('Submit', disabled=not (uploaded_file and query_text))
+    submitted = st.form_submit_button('Submit', key='submit_button', help='Click to submit the query')
     if submitted and openai_api_key.startswith('sk-'):
         with st.spinner('Loading response...'):
             # Add the user query to the conversation history
