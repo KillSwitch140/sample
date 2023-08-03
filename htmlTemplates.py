35e01bb1e92097666b7df9ba9438124690b03d55
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-08-04 01:00:57 +0530

    Create htmlTemplates.py

---
+++ htmlTemplates.py
@@ -0,0 +1,57 @@
+css = '''
+<style>
+    .chat-container {
+        height: 25px;
+        overflow-y: scroll;
+    }
+    .user-bubble {
+        display: flex;
+        justify-content: flex-start;
+    }
+    .user-bubble > div {
+        padding: 15px;
+        background-color: #e0e0e0;
+        border-radius: 10px;
+        width: 50%;
+        margin-left: 50%;
+    }
+    .assistant-bubble {
+        display: flex;
+        justify-content: flex-end;
+    }
+    .assistant-bubble > div {
+        padding: 15px;
+        background-color: #0078d4;
+        color: white;
+        border-radius: 10px;
+        width: 50%;
+        margin-right: 50%;
+    }
+    .chat-input-prompt {
+        position: sticky;
+        bottom: 0;
+        background-color: white;
+        padding: 10px;
+        width: 100%;
+    }
+    .chat-header {
+        position: sticky;
+        top: 0;
+        background-color: #f2f2f2;
+        padding: 10px;
+        width: 100%;
+    }
+</style>
+'''
+
+bot_template = '''
+<div class="assistant-bubble">
+    <div>{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="user-bubble">
+    <div>{{MSG}}</div>
+</div>
+'''
