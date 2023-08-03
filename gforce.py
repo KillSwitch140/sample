076eaa99447ca0ed3ee7c5f46a63fc1f827c6c10
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-08-03 09:46:38 +0530

    Update gforce.py

--- gforce.py
+++ gforce.py
@@ -1,11 +1,20 @@
 import streamlit as st
-import openai
+import os
 import PyPDF2
+import re
+import spacy
+import openai
+
 
 # Set up your OpenAI API key from Streamlit secrets
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
-# Function to read PDF text
+# Connect to the database and create the table
+database_name = "resumes.db"
+connection = create_connection(database_name)
+create_resumes_table(connection)
+
+
 def read_pdf_text(uploaded_file):
     pdf_reader = PyPDF2.PdfReader(uploaded_file)
     text = ""
@@ -15,22 +24,78 @@ def read_pdf_text(uploaded_file):
 
     return text
 
+# Function to extract GPA using regular expression
+def extract_gpa(text):
+    gpa_pattern = r"\bGPA\b\s*:\s*([\d.]+)"
+    gpa_match = re.search(gpa_pattern, text, re.IGNORECASE)
+    return gpa_match.group(1) if gpa_match else None
+
+# Function to extract email using regular expression
+def extract_email(text):
+    email_pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+    email_match = re.search(email_pattern, text)
+    return email_match.group() if email_match else None
+
 # Initialize conversation history in session state
 if "conversation_history" not in st.session_state:
-    st.session_state.conversation_history = [{'role': 'system', 'content': 'Hello! I am your recruiter assistant. My role is to go through resumes and help recruiters make informed decisions.'}]
+    st.session_state.conversation_history = []
 
-def generate_response(openai_api_key, query_text, uploaded_files):
+# Function to extract candidate name using spaCy NER
+def extract_candidate_name(resume_text):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(resume_text)
+    candidate_name = None
+    for ent in doc.ents:
+        if ent.label_ == "PERSON":
+            candidate_name = ent.text
+            break
+    return candidate_name
+
+# Page title and styling
+st.set_page_config(page_title='GForce Resume Reader', layout='wide')
+st.title('GForce Resume Reader')
+
+# List to store uploaded resume contents and extracted information
+uploaded_resumes = []
+candidates_info = []
+
+# File upload
+uploaded_files = st.file_uploader('Please upload your resume', type='pdf', accept_multiple_files=True)
+
+# Process uploaded resumes and store in the database
+if uploaded_files:
+    for uploaded_file in uploaded_files:
+        if uploaded_file is not None:
+            resume_text = read_pdf_text(uploaded_file)
+            uploaded_resumes.append(resume_text)
+            # Extract GPA, email, and past
+            gpa = extract_gpa(resume_text)
+            email = extract_email(resume_text)
+            # Extract candidate name using spaCy NER
+            candidate_name = extract_candidate_name(resume_text)
+            # Store the information for each candidate
+            candidate_info = {
+                'name': candidate_name,
+                'gpa': gpa,
+                'email': email,
+                'resume_text': resume_text
+            }
+            candidates_info.append(candidate_info)
+            # Store the resume and information in the database
+            insert_resume(connection, candidate_info)
+
+def generate_response(openai_api_key, query_text, candidates_info):
     # Load document if file is uploaded
-    if uploaded_files:
+    if len(candidates_info) > 0:
         # Prepare the conversation history with user query
         conversation_history = [{'role': 'user', 'content': query_text}]
 
-        # Process each uploaded resume separately
-        for idx, uploaded_file in enumerate(uploaded_files):
-            resume_text = read_pdf_text(uploaded_file)
-
-            # Append the resume text to the conversation history
+        # Process each resume separately and store the summaries in candidates_info
+        for idx, candidate_info in enumerate(candidates_info):
+            resume_text = candidate_info["resume_text"]
+            # Append the summarized resume text to the conversation history
             conversation_history.append({'role': 'system', 'content': f'Resume {idx + 1}: {resume_text}'})
+
 
         # Generate the response using the updated conversation history
         response = openai.ChatCompletion.create(
@@ -43,20 +108,8 @@ def generate_response(openai_api_key, query_text, uploaded_files):
         return assistant_response
 
     else:
-        return "Sorry, no resumes uploaded. Please upload resumes first."
+        return "Sorry, no resumes found in the database. Please upload resumes first."
 
-
-# Page title and styling
-st.set_page_config(page_title='GForce Resume Reader', layout='wide')
-st.title('GForce Resume Reader')
-
-# Sidebar for job details
-st.sidebar.title('Job Details')
-job_title = st.sidebar.text_input("Enter the job title:")
-qualifications = st.sidebar.text_area("Enter the qualifications for the job (separated by commas):")
-
-# File upload
-uploaded_files = st.file_uploader('Please upload your resume', type='pdf', accept_multiple_files=True)
 
 # User query
 user_query = st.text_area('You (Type your message here):', value='', help='Ask away!', height=100, key="user_input")
@@ -66,12 +119,16 @@ send_user_query = st.button('Send', help='Click to submit the query', key="send_
 if send_user_query:
     if user_query.strip() != '':
         with st.spinner('Chatbot is typing...'):
-            # Generate the response using the user query
-            response = generate_response(openai_api_key, user_query, st.session_state.conversation_history)
-            # Append the user's and assistant's messages to the conversation history
+            # Add the user query to the conversation history
             st.session_state.conversation_history.append({'role': 'user', 'content': user_query})
+            # Get the updated conversation history
+            conversation_history = st.session_state.conversation_history.copy()
+            # Append the uploaded resumes' content to the conversation history
+            conversation_history.extend([{'role': 'system', 'content': resume_text} for resume_text in uploaded_resumes])
+            # Generate the response using the updated conversation history
+            response = generate_response(openai_api_key, user_query, candidates_info)
+            # Append the assistant's response to the conversation history
             st.session_state.conversation_history.append({'role': 'assistant', 'content': response})
-
 # Chat UI with sticky headers and input prompt
 st.markdown("""
 <style>
