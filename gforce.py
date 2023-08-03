0978558f1f677354f132b025bda8cb34e3b46928
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-08-04 00:35:33 +0530

    Update gforce.py

--- gforce.py
+++ gforce.py
@@ -31,6 +31,68 @@ from langchain.document_loaders import PyPDFLoader
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
+# CSS styles for the chat messages
+chat_styles = """
+<style>
+    .chat-container {
+        height: 400px;
+        overflow-y: scroll;
+    }
+    .user-bubble {
+        display: flex;
+        justify-content: flex-start;
+    }
+    .user-bubble > div {
+        padding: 15px;
+        background-color: #e0e0e0;
+        border-radius: 10px;
+        width: 50%;
+        margin-left: 50%;
+    }
+    .assistant-bubble {
+        display: flex;
+        justify-content: flex-end;
+    }
+    .assistant-bubble > div {
+        padding: 15px;
+        background-color: #0078d4;
+        color: white;
+        border-radius: 10px;
+        width: 50%;
+        margin-right: 50%;
+    }
+    .chat-input-prompt {
+        position: sticky;
+        bottom: 0;
+        background-color: white;
+        padding: 10px;
+        width: 100%;
+    }
+    .chat-header {
+        position: sticky;
+        top: 0;
+        background-color: #f2f2f2;
+        padding: 10px;
+        width: 100%;
+    }
+</style>
+"""
+
+# Function to display the chat messages
+def display_chat_message(message, role):
+    if role == "user":
+        st.markdown("""
+        <div class="user-bubble">
+            <div>{}</div>
+        </div>
+        """.format(message), unsafe_allow_html=True)
+    elif role == "assistant":
+        st.markdown("""
+        <div class="assistant-bubble">
+            <div>{}</div>
+        </div>
+        """.format(message), unsafe_allow_html=True)
+
 def read_pdf(uploaded_files):
     text = ""
 
@@ -41,55 +103,42 @@ def read_pdf(uploaded_files):
 
     return text
 
-
 def get_text_chunks(file):
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     chunks = text_splitter.create_documents(file)
     return chunks
 
 def get_vectorstore(text_chunks):
-   embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-   # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-   vectorstore =  Chroma.from_documents(text_chunks, embeddings)
-   return vectorstore 
-    
-    
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    vectorstore = Chroma.from_documents(text_chunks, embeddings)
+    return vectorstore
+
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.1)
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorstore.as_retriever(),
         memory=memory
     )
-    return conversation_chain    
+    return conversation_chain
 
 def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
 
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-            
-
-# Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "Hi, I am your resume Q&A bot. How can I help you today?"}]
-
-
+    st.markdown('<div class="chat-container">', unsafe_allow_html=True)
+    for message in st.session_state.chat_history:
+        display_chat_message(message.content, message.role)
+    st.markdown('</div>', unsafe_allow_html=True)
 
 def main():
-    # Page title
     st.set_page_config(page_title='Gforce Resume Assistant', layout='wide')
     st.title('Gforce Resume Assistant')
+
+    st.markdown(chat_styles, unsafe_allow_html=True)
+
+    openai_api_key = st.secrets["OPENAI_API_KEY"]
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
@@ -107,7 +156,7 @@ def main():
             "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing"):
-               # get pdf text
+                # get pdf text
                 raw_text = read_pdf(pdf_docs)
 
                 # get the text chunks
@@ -117,9 +166,7 @@ def main():
                 vectorstore = get_vectorstore(text_chunks)
 
                 # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
-
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
 if __name__ == '__main__':
     main()
