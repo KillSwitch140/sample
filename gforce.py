7576ef429c96db5d320f59c6bc7944fe48ba553b
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-08-05 00:28:46 +0530

    Update gforce.py

--- gforce.py
+++ gforce.py
@@ -160,10 +160,11 @@ if schedule_button and person_name and person_email and date_time:
     # Check if the meeting has been successfully scheduled
     if meeting_scheduled:
         agent.run(send_email)
+    # Print or display the combined string
+    st.sidebar.success("Interview Scheduled Successfully!")
 
 
     
 
-# Print or display the combined string
-st.sidebar.success("Interview Scheduled Successfully!")
+
 
