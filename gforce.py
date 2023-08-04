f8b87f90bee57a7057d33661084d444cf0c9580b
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-08-04 19:34:16 +0530

    Update gforce.py

--- gforce.py
+++ gforce.py
@@ -56,18 +56,20 @@ def generate_response(doc_texts, openai_api_key, query_text):
     retriever = db.as_retriever()
     #Bot memory
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages="True")
-    prompt = """
+    template  = """
             You are an AI assistant created to help hiring managers review resumes and shortlist candidates. You have been provided with resumes and job descriptions to review. When asked questions, use the provided documents to provide helpful and relevant information to assist the hiring manager. Be concise, polite and professional. Do not provide any additional commentary or opinions beyond answering the questions directly based on the provided documents.
             """
+    QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
     # Create QA chain 
-    qa = RetrievalQA.from_chain_type(
-    llm=llm, 
-    chain_type='stuff', 
-    retriever=retriever,
-    chain_type_kwargs={"prompt": prompt})
+    qa_chain = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectorstore.as_retriever(),
+    chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
+)
+
 
     
-    response = qa_chain.run(query_text)
+    response = qa_chain.run({"query": query_text})
     
     return response
     
