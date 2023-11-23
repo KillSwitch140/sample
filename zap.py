0d250a4896895972453ab0eac3ee1c9492c70d05
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-11-23 18:41:01 -0500

    Update zap.py

--- zap.py
+++ zap.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from langchain.llms import OpenAI
-from langchain.agents import initialize_agent
+from langchain.agents import AgentType, initialize_agent
 from langchain.agents.agent_toolkits import ZapierToolkit
+from langchain.llms import OpenAI
 from langchain.utilities.zapier import ZapierNLAWrapper
 import os
 from os import environ
@@ -15,7 +15,9 @@ openai_api_key = st.secrets["OPENAI_API_KEY"]
 llm = OpenAI(temperature=0)
 zapier = ZapierNLAWrapper()
 toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
-agent = initialize_agent(toolkit.get_tools(), llm, agent="zero-shot-react-description", verbose=True)
+agent = agent = initialize_agent(
+    toolkit.get_tools(), llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+)
 
 def schedule_interview(person_name, person_email, date, time):
     # Create the combined string
